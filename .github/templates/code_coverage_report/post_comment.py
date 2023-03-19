6c3738d156da3b43e98a4e6681bdc8e9fa6f6ed1
Jasper Ginn <jasperginn@gmail.com>
2023-03-19 10:03:43 +0100

    feat(build): add template action

---
+++ .github/templates/code_coverage_report/post_comment.py
@@ -0,0 +1,51 @@
+#%%
+
+import markdown
+
+import subprocess
+import argparse
+import requests
+import json
+import logging
+
+logger = logging.getLogger("code_coverage_reporter")
+handler = logging.StreamHandler()
+format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(format)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def main(endpoint: str, token: str):
+    out = subprocess.check_output(
+        ["poetry", "run", "interrogate", "-v", "../../../.template/mypkg"]
+    )
+    out_spl = out.decode('utf-8').split('\n')
+    out_spl[-2] = out_spl[-2].replace('-', '').strip().replace("RESULT:", "**RESULT**:")
+    del out_spl[-4]
+    out_spl = "\n".join(out_spl[2:])
+    table = markdown.markdown(out_spl, extensions=["markdown.extensions.tables"])
+    msg = f"""<h3>Docstring coverage</h3><br/>
+    {table}"""
+    body={"body": msg}
+    r = requests.post(
+        url=endpoint,
+        headers={
+            'Authorization': f'token {token}'
+        },
+        data=json.dumps(body)
+    )
+    r.raise_for_status()
+    
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser(
+        prog="CodeCoverageReporter",
+        description="Use this in GH actions to post the results of interrogate code coverage <https://github.com/econchick/interrogate> to a PR as a comment"
+    )
+    parser.add_argument('github_repository', type=str)
+    parser.add_argument('pr_number', type=int)
+    parser.add_argument('token', type=str)
+    args=parser.parse_args()
+    endpoint=f'https://api.github.com/repos/{args.github_repository}/issues/{args.pr_number}/comments'
+    main(endpoint, args.token)
